d40f9ab4f2fd218598fe35e9e170addd1aa3db69
MeMeZ <644770257@qq.com>
2025-09-19 12:59:38 +0800

    Update Words.lua

--- Words.lua
+++ Words.lua
@@ -1,6 +1,9 @@
 return {
-    ["KillAura"] = "杀戮光环",
-    ["Kill Aura"] = "杀戮光环",
-    ["Range"] = "距离",
-    ["WalkSpeed"] = "移动速度",
+    ["killaura"] = "杀戮光环",
+    ["kill aura"] = "杀戮光环",
+    ["silent aim"] = "子弹追踪",
+    ["range"] = "距离",
+    ["walkspeed"] = "移动速度",
+    ["godmode"] = "无敌",
+    ["fly"] = "飞行",
 }
